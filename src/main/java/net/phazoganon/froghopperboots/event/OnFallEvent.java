5df26bffccb79ce396475c5eec35bbeb9c2182cf
Armasher5872 <armasher1505@gmail.com>
2025-03-07 20:16:11 -0500

    More code

--- src/main/java/net/phazoganon/froghopperboots/event/OnFallEvent.java
+++ src/main/java/net/phazoganon/froghopperboots/event/OnFallEvent.java
@@ -2,6 +2,8 @@ package net.phazoganon.froghopperboots.event;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -11,16 +13,14 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingFallEvent;
 import net.phazoganon.froghopperboots.FrogHopperBoots;
 import net.phazoganon.froghopperboots.item.ModItems;
-import net.phazoganon.froghopperboots.util.ModUtils;
-
-import static net.phazoganon.froghopperboots.util.ModUtils.addEntityHandler;
 
 @EventBusSubscriber(modid = FrogHopperBoots.MODID)
 public class OnFallEvent {
+    private static int boundCount;
     @SubscribeEvent
     public static void onFall(LivingFallEvent event) {
         LivingEntity livingEntity = event.getEntity();
-        if (livingEntity.fallDistance > 0) {
+        if (livingEntity.fallDistance > 1) {
             if (livingEntity.getItemBySlot(EquipmentSlot.FEET).is(ModItems.FROG_HOPPER_BOOTS)) {
                 if (!livingEntity.isSuppressingBounce()) {
                     event.setDamageMultiplier(0.0F);
@@ -29,14 +29,18 @@ public class OnFallEvent {
                         double gravity = livingEntity.getAttributeValue(Attributes.GRAVITY);
                         double time = Math.sqrt(livingEntity.fallDistance/gravity);
                         double velocity = gravity*time;
-                        livingEntity.setDeltaMovement(vec3.x/0.975F, velocity, vec3.z/0.975F);
+                        livingEntity.setDeltaMovement(vec3.x, velocity, vec3.z);
                         livingEntity.hurtMarked = true;
-                        addEntityHandler(livingEntity);
+                        if (boundCount <= 0) {
+                            boundCount++;
+                        }
+                        if (boundCount > 0) {
+                            livingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 25, 126, true, true));
+                        }
                     }
                     else {
                         float factor = livingEntity.fallDistance < 2 ? -0.7f : -0.9f;
                         livingEntity.setDeltaMovement(vec3.x/0.975f, vec3.y*factor, vec3.z/0.975f);
-                        ModUtils.addEntityHandler(livingEntity, livingEntity.getDeltaMovement());
                     }
                     event.setDistance(0.0F);
                     if (!livingEntity.level().isClientSide) {
@@ -50,6 +54,9 @@ public class OnFallEvent {
                     event.setDamageMultiplier(0.2F);
                 }
             }
+        }
+        else {
+            boundCount = 0;
         }
     }
 }
